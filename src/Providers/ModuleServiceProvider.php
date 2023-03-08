38841484042837ddef893d5bc08b54a2d7cbb5da
Fahrettin Aksoy <aksoy@fahrettinaksoy.com.tr>
2023-03-08 20:34:32 +0300

    package configuration

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WeeynSoft\CatalogCategory\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+	
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'name' => 'Weeyn Category Module',
+	'version' => '1.0.0'
+];
